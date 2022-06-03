b121fc3b7d0558c2a6cdf7a0701ae243f079c208
Lrc123 <892834414@qq.com>
2022-06-02 17:16:17 -0700

    Logging

--- MyLittleGameEngine/src/GG.h
+++ MyLittleGameEngine/src/GG.h
@@ -6,7 +6,11 @@
 #include <stdio.h>
 #include "GG/Application.h"
 
+// --- Log ---------
+#include "GG/Log.h"
+
 // ---Entry Point---------
 
 #include "GG/EntryPoint.h"
 // -----------------------
+
--- MyLittleGameEngine/src/GG/EntryPoint.h
+++ MyLittleGameEngine/src/GG/EntryPoint.h
@@ -7,6 +7,10 @@ extern GG::Application* GG::CreateApplication();
 
 int main(int argc, char** argv) {
 	printf("Good game Engine\n");
+	GG::Log::Init();
+	GG_CORE_WARN("Initialized Log!");
+	int a = 5;
+	GG_INFO("Hello Client Log! Var={0}", a);
 	auto app = GG::CreateApplication();
 	app->Run();
 	delete app;
